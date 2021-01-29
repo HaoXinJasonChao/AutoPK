149b9281140def3bf3780f52831da1297e0eb70c
Jason - Hao Xin Chao <72322931+Jason-HaoXinChao@users.noreply.github.com>
2021-01-28 19:36:36 -0500

    added server code

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 "use strict"
-
+const log = console.log;
 const express = require("express");
-
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-const PORT = express.env.PORT || 5000;
+
+app.use(express.static(__dirname + "/pub"));
+
+app.get('/', (req, res) => {
+    res.send('root route');
+});
+
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}.`);
